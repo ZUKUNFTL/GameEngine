390ef81d6e8dca5688a6d1d464094964c986b74e
wuhaoluo <370536448@qq.com>
2023-10-10 16:24:13 +0800

    将opengl的VBO,EBO抽象出来

---
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.cpp
@@ -0,0 +1,68 @@
+#include "hzpch.h"
+#include "OpenGLBuffer.h"
+
+#include <glad/glad.h>
+namespace Hazel {
+	
+	/////////////////////////////////////////////////////////////////////////////
+	// VertexBuffer /////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
+	OpenGLVertexBuffer::OpenGLVertexBuffer(float* vertices, uint32_t size)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+		/*
+		@brief
+		glBufferData是一个专门用来把用户定义的数据复制到当前绑定缓冲的函数。
+		它的第一个参数是目标缓冲的类型：顶点缓冲对象当前绑定到GL_ARRAY_BUFFER目标上。
+		第二个参数指定传输数据的大小(以字节为单位)；用一个简单的sizeof计算出就行。
+		第三个参数是我们希望发送的实际数据。
+		第四个参数指定了我们希望显卡如何管理给定的数据。它有三种形式：
+		GL_STATIC_DRAW ：数据不会或几乎不会改变。
+		GL_DYNAMIC_DRAW：数据会被改变很多。
+		GL_STREAM_DRAW ：数据每次绘制时都会改变。
+		*/
+		glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+	}
+
+	OpenGLVertexBuffer::~OpenGLVertexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGLVertexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void OpenGLVertexBuffer::Unbind() const
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	// IndexBuffer //////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////////
+	OpenGLIndexBuffer::OpenGLIndexBuffer(uint32_t* indices, uint32_t count)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	}
+
+	OpenGLIndexBuffer::~OpenGLIndexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+	void OpenGLIndexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void OpenGLIndexBuffer::Unbind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+}
