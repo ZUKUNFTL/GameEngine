f2e7ff0629fa97e001647eff831941509771a344
zukunft <370536448@qq.com>
2023-10-14 15:55:36 +0800

    整个渲染架构的搭建

--- Hazel/src/Hazel/Renderer/Renderer.h
+++ Hazel/src/Hazel/Renderer/Renderer.h
@@ -1,17 +1,20 @@
 #pragma once
+#include "RenderCommand.h"
+
+//Renderer是一个高级的渲染类，它将处理场景，网络和高级结构
 
 namespace Hazel {
-
-	enum class RendererAPI
-	{
-		None=0, OpenGL=1
-	};
 
 	class Renderer
 	{
 	public:
-		inline static RendererAPI GetAPI() { return s_RendererAPI; }
-	private:
-		static RendererAPI s_RendererAPI;
+		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
+		// TODO: 未来会接受Scene场景的相关参数，比如Camera、lighting, 保证shaders能够得到正确的环境相关的uniforms
+		static void BeginScene();
+		// TODO:
+		static void EndScene();
+		// TODO: 会把VAO通过RenderCommand下的指令，传递给RenderCommandQueue
+		// 目前偷个懒，直接调用RenderCommand::DrawIndexed()函数
+		static void Submit(const std::shared_ptr<VertexArray>& vertexArray);
 	};
 }
