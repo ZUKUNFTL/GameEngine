85c1fd4353e28fdfcdde154b6982c7cb57167b8b
zukunft <370536448@qq.com>
2023-10-22 10:47:26 +0800

    添加blending

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -6,6 +6,8 @@ namespace Hazel {
 	class OpenGLRendererAPI :public RendererAPI
 	{
 	public:
+
+		virtual void Init();
 		virtual void Clear() const;
 		virtual void SetClearColor(const glm::vec4&) const;
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray);
