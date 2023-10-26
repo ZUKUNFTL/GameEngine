728c1f3b7265768a9f652a659fb7a9eee9f100ed
zukunft <370536448@qq.com>
2023-10-26 23:11:23 +0800

    添加窗口Resize功能

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -11,6 +11,12 @@ namespace Hazel {
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
+	void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+	{
+		//绘图的区域改变,改变缓冲区，实现放大缩小后渲染不同的区域
+		glViewport(x, y, width, height);
+	}
+
 	void OpenGLRendererAPI::Clear() const
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -8,6 +8,7 @@ namespace Hazel {
 	public:
 
 		virtual void Init();
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 		virtual void Clear() const;
 		virtual void SetClearColor(const glm::vec4&) const;
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray);
