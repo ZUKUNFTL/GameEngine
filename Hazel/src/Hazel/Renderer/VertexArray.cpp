b145052bf1ac9a276316580e8b23b4b4ffa5d4f6
zukunft <370536448@qq.com>
2023-10-31 23:35:40 +0800

    添加2D渲染类

--- Hazel/src/Hazel/Renderer/VertexArray.cpp
+++ Hazel/src/Hazel/Renderer/VertexArray.cpp
@@ -6,13 +6,13 @@
 
 namespace Hazel {
 
-	Hazel::VertexArray* VertexArray::Create()
+	Ref<VertexArray> VertexArray::Create()
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None: HZ_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 			//#if HZ_PLATFORM_WINDOWS
-		case RendererAPI::API::OpenGL: return new OpenGLVertexArray();
+		case RendererAPI::API::OpenGL: return std::make_shared<OpenGLVertexArray>();
 			//#endif
 		}
 
