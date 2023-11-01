f9266f3d22945be5177c394eb6cfb22c2efbf2cc
zukunft <370536448@qq.com>
2023-11-01 22:35:07 +0800

    添加2DTransform

--- Hazel/src/Hazel/Renderer/Shader.h
+++ Hazel/src/Hazel/Renderer/Shader.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <glm/glm.hpp>
 
 namespace Hazel {
 
@@ -10,6 +11,12 @@ namespace Hazel {
 
 		virtual void Bind() const = 0;
 		virtual void UnBind() const = 0;
+
+		virtual void SetInt(const std::string& name, int value) = 0;
+		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+		virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
+
 		virtual std::string GetName() const = 0;
 
 		static Ref<Shader> Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
