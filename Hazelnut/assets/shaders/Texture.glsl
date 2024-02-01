354bdd3fbf8628342630bad3ad84a0b36e4c843f
zukunft <370536448@qq.com>
2024-02-01 15:23:45 +0800

    PlayStop按钮

--- Hazelnut/assets/shaders/Texture.glsl
+++ Hazelnut/assets/shaders/Texture.glsl
@@ -19,19 +19,20 @@ struct VertexOutput
 {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
+	//float TexIndex; // 移出struct
 	float TilingFactor;
 };
 
 //为4是因为结构体也占大小
 layout (location = 0) out VertexOutput Output;
+layout(location = 3) out flat float v_TexIndex; // 移到这里
 layout (location = 4) out flat int v_EntityID;
 
 void main()
 {
 	Output.Color = a_Color;
 	Output.TexCoord = a_TexCoord;
-	Output.TexIndex = a_TexIndex;
+	v_TexIndex = a_TexIndex;
 	Output.TilingFactor = a_TilingFactor;
 	v_EntityID = a_EntityID;
 
@@ -48,11 +49,11 @@ struct VertexOutput
 {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout (location = 0) in VertexOutput Input;
+layout(location = 3) in flat float v_TexIndex;
 layout (location = 4) in flat int v_EntityID;
 
 layout (binding = 0) uniform sampler2D u_Textures[32];
@@ -61,7 +62,7 @@ void main()
 {
 	vec4 texColor = Input.Color;
 
-	switch(int(Input.TexIndex))
+	switch(int(v_TexIndex))
 	{
 		case  0: texColor *= texture(u_Textures[ 0], Input.TexCoord * Input.TilingFactor); break;
 		case  1: texColor *= texture(u_Textures[ 1], Input.TexCoord * Input.TilingFactor); break;
