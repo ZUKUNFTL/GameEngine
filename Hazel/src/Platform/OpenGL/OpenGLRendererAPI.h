27ebeca39b2ebd4b8fa8b9ca7e56c4387fe7e905
zukunft <370536448@qq.com>
2024-02-04 19:49:17 +0800

    渲染Line和Rect

--- Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ Hazel/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -6,11 +6,13 @@ namespace Hazel {
 	class OpenGLRendererAPI :public RendererAPI
 	{
 	public:
-
 		virtual void Init();
 		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 		virtual void Clear();
 		virtual void SetClearColor(const glm::vec4&);
 		virtual void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0);
+		virtual void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount) override;
+
+		virtual void SetLineWidth(float width) override;
 	};
 }
